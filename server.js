d55288ca9a9fb893fb4ddda98705e43bcc258ec2
tuankiet-17ti-20hcmus <kietdeptrai0404@gmail.com>
2022-12-12 15:20:29 +0700

    add env

--- server.js
+++ server.js
@@ -148,6 +148,7 @@ app.post('/register', async (req, res) => {
 	res.json({ status: 'ok' })
 })
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000
+app.listen(port, () => {
 	console.log('Server up at http://localhost:3000/')
 })
