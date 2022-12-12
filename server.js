ec84de5e4ea319644070a4754fd89fce05473d89
tuankiet-17ti-20hcmus <kietdeptrai0404@gmail.com>
2022-12-12 16:45:26 +0700

    reload

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ const User = require('./model/user')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
-// async function connect() {
-//     try {
-//         await mongoose.connect('mongodb://localhost:27017/login-app-db');
-//         console.log('connect database success');
-//     }
-//     catch (error) {
-//         console.log('connect fail');
+async function connect() {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/login-app-db');
+        console.log('connect database success');
+    }
+    catch (error) {
+        console.log('connect fail');
 
-//     }
-// }
+    }
+}
 
-// connect()
+connect()
 
 const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
 
@@ -40,115 +40,116 @@ app.get('/', (reg, res) => {
 	res.render('register')
 })
 
-// app.get('/login', (reg, res) => {
-// 	res.render('login')
-// })
+app.get('/login', (reg, res) => {
+	res.render('login')
+})
 
-// app.get('/change-password', (reg, res) => {
-// 	res.render('change-password')
-// })
+app.get('/change-password', (reg, res) => {
+	res.render('change-password')
+})
 
-// app.post('/', (reg, res) => {
-// 	res.render('home')
-// })
+app.post('/', (reg, res) => {
+	res.render('home')
+})
 
-// app.post('/api/change-password', async (req, res) => {
-// 	const { token, newpassword: plainTextPassword } = req.body
+app.post('/api/change-password', async (req, res) => {
+	const { token, newpassword: plainTextPassword } = req.body
 
-// 	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
-// 		return res.json({ status: 'error', error: 'Invalid password' })
-// 	}
+	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
 
-// 	if (plainTextPassword.length < 5) {
-// 		return res.json({
-// 			status: 'error',
-// 			error: 'Password too small. Should be atleast 6 characters'
-// 		})
-// 	}
+	if (plainTextPassword.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be atleast 6 characters'
+		})
+	}
 
-// 	try {
-// 		const user = jwt.verify(token, JWT_SECRET)
+	try {
+		const user = jwt.verify(token, JWT_SECRET)
 
-// 		const _id = user.id
+		const _id = user.id
 
-// 		const password = await bcrypt.hash(plainTextPassword, 10)
+		const password = await bcrypt.hash(plainTextPassword, 10)
 
-// 		await User.updateOne(
-// 			{ _id },
-// 			{
-// 				$set: { password }
-// 			}
-// 		)
-// 		res.json({ status: 'ok' })
-// 	} catch (error) {
-// 		console.log(error)
-// 		res.json({ status: 'error', error: ';))' })
-// 	}
-// })
+		await User.updateOne(
+			{ _id },
+			{
+				$set: { password }
+			}
+		)
+		res.json({ status: 'ok' })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: ';))' })
+	}
+})
 
-// app.post('/api/login', async (req, res) => {
-// 	const { username, password } = req.body
-// 	const user = await User.findOne({ username }).lean()
+app.post('/api/login', async (req, res) => {
+	const { username, password } = req.body
+	const user = await User.findOne({ username }).lean()
 
-// 	if (!user) {
-// 		return res.json({ status: 'error', error: 'Invalid username/password' })
-// 	}
+	if (!user) {
+		return res.json({ status: 'error', error: 'Invalid username/password' })
+	}
 
-// 	if (await bcrypt.compare(password, user.password)) {
-// 		// the username, password combination is successful
+	if (await bcrypt.compare(password, user.password)) {
+		// the username, password combination is successful
 
-// 		const token = jwt.sign(
-// 			{
-// 				id: user._id,
-// 				username: user.username
-// 			},
-// 			JWT_SECRET
-// 		)
+		const token = jwt.sign(
+			{
+				id: user._id,
+				username: user.username
+			},
+			JWT_SECRET
+		)
 
-// 		return res.json({ status: 'ok', data: token })
-// 	}
+		return res.json({ status: 'ok', data: token })
+	}
 
-// 	res.json({ status: 'error', error: 'Wrong username or password' })
-// })
+	res.json({ status: 'error', error: 'Wrong username or password' })
+})
 
-// app.post('/register', async (req, res) => {
-// 	const { username, password: plainTextPassword } = req.body
+app.post('/register', async (req, res) => {
+	const { username, password: plainTextPassword } = req.body
 
-// 	if (!username || typeof username !== 'string') {
-// 		return res.json({ status: 'error', error: 'Invalid username' })
-// 	}
+	if (!username || typeof username !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid username' })
+	}
 
-// 	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
-// 		return res.json({ status: 'error', error: 'Invalid password' })
-// 	}
+	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
 
-// 	if (plainTextPassword.length < 5) {
-// 		return res.json({
-// 			status: 'error',
-// 			error: 'Password too small. Should be atleast 6 characters'
-// 		})
-// 	}
+	if (plainTextPassword.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be atleast 6 characters'
+		})
+	}
 
-// 	const password = await bcrypt.hash(plainTextPassword, 10)
+	const password = await bcrypt.hash(plainTextPassword, 10)
 
-// 	try {
-// 		const response = await User.create({
-// 			username,
-// 			password
-// 		})
-// 		console.log('User created successfully: ', response)
-// 	} catch (error) {
-// 		if (error.code === 11000) {
-// 			// duplicate key
-// 			return res.json({ status: 'error', error: 'Username already in use' })
-// 		}
-// 		throw error
-// 	}
+	try {
+		const response = await User.create({
+			username,
+			password
+		})
+		console.log('User created successfully: ', response)
+	} catch (error) {
+		if (error.code === 11000) {
+			// duplicate key
+			return res.json({ status: 'error', error: 'Username already in use' })
+		}
+		throw error
+	}
 
-// 	res.json({ status: 'ok' })
-// })
+	res.json({ status: 'ok' })
+})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
+	console.log(process.env)
 	console.log('Server up at http://localhost:3000/')
 })
